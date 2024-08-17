3d20cb820bd46c976446e3660293f205c4823171
Liliana Bassa <lilianabassa0510@gmail.com>
2024-08-17 21:23:12 +0200

    Register User and UserProfile

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile  
 
-# Register your models here.
+# Inline profile info to user
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+
+# Custom UserAdmin
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-register with the new UserAdmin
+admin.site.register(User, UserAdmin)
+

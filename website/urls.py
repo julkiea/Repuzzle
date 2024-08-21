5f4a7667e5f9a627750f8ed99bfc6216aa563d11
Liliana Bassa <lilianabassa0510@gmail.com>
2024-08-21 15:59:03 +0200

    Create new url: add_puzzle

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name ='home'),
     path('register_user/', views.register_user, name = 'register_user'),
-    path('login_user', views.login_user, name = 'login_user'),
-    path('logout_user', views.logout_user, name = 'logout_user')
+    path('login_user/', views.login_user, name = 'login_user'),
+    path('logout_user/', views.logout_user, name = 'logout_user'),
+    path('add_puzzle/', views.add_puzzle, name = 'add_puzzle')
 ]

61de205619835b3eb06af068e4fbb5ff5eade9f2
Liliana Bassa <lilianabassa0510@gmail.com>
2024-08-27 22:37:39 +0200

    Add new urls: puzzle_list, puzzle

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('register_user/', views.register_user, name = 'register_user'),
     path('login_user/', views.login_user, name = 'login_user'),
     path('logout_user/', views.logout_user, name = 'logout_user'),
-    path('add_puzzle/', views.add_puzzle, name = 'add_puzzle')
+    path('add_puzzle/', views.add_puzzle, name = 'add_puzzle'),
+    path('puzzle_list/', views.puzzle_list, name='puzzle_list'),
+    path('puzzle/<int:pk>', views.puzzle, name= 'puzzle'),
 ]
